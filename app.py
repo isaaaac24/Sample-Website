2e237efdde20097797ab2ab74d0db2289affee58
isaaaac24 <zacwrightson@gmail.com>
2022-01-31 18:25:15 +0000

    Test

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+import socket
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    my_host = "127.0.0.1"
+    free_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    free_socket.bind((my_host, 0))
+    free_socket.listen(5)
+    free_port = free_socket.getsockname()[1]
+    free_socket.close()
+
+    app.run(host=my_host, port=free_port, debug=True)
